9a10e8bce0189a538ed24b511e49503769fda5c2
muqaddasazahra <muqaddasazahra@gmail.com>
2024-01-02 00:11:10 +0500

    Database connection

---
+++ Database/config.js
@@ -0,0 +1,23 @@
+import { Sequelize } from "sequelize";
+
+
+const sequelize= new Sequelize("todo", "postgres", "admin", {
+    host:"localhost",
+    port:5432,
+    dialect:"postgres",
+    logging: false,
+})
+
+const connectDB= async()=>
+{
+    try{
+        await sequelize.authenticate();
+        console.log("Connection has been established successfully");
+    }
+    catch(err)
+    {
+        console.log("Trouble connecting to the database  "+err);
+    }
+}
+
+export default connectDB;
