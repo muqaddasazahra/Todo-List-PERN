d903a011ef3857468d1a80355f07c98a8ac2138a
muqaddasazahra <muqaddasazahra@gmail.com>
2024-01-02 00:06:09 +0500

    Basic Express app setup

---
+++ app.js
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from "cors";
+import connectDB from "./Database/config.js";
+
+const app=express();
+app.use(cors());
+app.use(express.json());
+
+app.listen(5000, ()=>
+{
+    console.log("Server started on port 5000");
+});
+
+connectDB();
