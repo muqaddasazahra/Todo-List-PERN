f6de299d1178b7a2de6c8f86d6d73aeab7694dce
muqaddasazahra <muqaddasazahra@gmail.com>
2024-01-02 11:57:57 +0500

    Created update, delete and getAllTodos Controller methods and Routes

--- Controller/TodoController.js
+++ Controller/TodoController.js
@@ -13,7 +13,56 @@ const TodoController={
     {
         res.status(400).send("couldn't create todo. Please try again later!");
     }
+    },
+
+    updateTodo: async (req,res)=>
+    {
+    const {description}=req.body;
+    const todo= await Todo.findByPk(req.params.id);
+    try 
+    {
+        await todo.update(
+            {
+                description: description,
+            }
+        );
+        res.status(200).send("Todo updated successfully");
     }
+    catch(err)
+    {
+        res.status(400).send("Todo couldn't be updated. Please try again later!"+err);
+    }
+    },
+
+    deleteTodo: async(req,res)=>
+    {
+        const todo= await Todo.findByPk(req.params.id);
+        try 
+    {
+        await todo.destroy();
+        res.status(200).send("Todo deleted successfully");
+    }
+    catch(err)
+    {
+        res.status(400).send("Todo couldn't be deleted. Please try again later!"+err);
+    }
+    },
+
+    getAllTodos: async (req,res)=>
+    {  
+        try
+        {
+            const todos=await Todo.findAll();
+            res.status(200).send(todos);
+        }
+        catch(err)
+        {
+            res.status(400).send("Couldn't get all todos. Please try again later!"+err);
+        }
+
+    }
+    
+
 }
 
 export default TodoController;
--- Routes/TodoRoutes.js
+++ Routes/TodoRoutes.js
@@ -5,5 +5,9 @@ import TodoController from "../Controller/TodoController.js";
 const TodoRouter=express.Router();
 
 TodoRouter.post("/create",TodoController.createTodo);
+TodoRouter.post("/update/:id",TodoController.updateTodo);
+TodoRouter.delete("/delete/:id",TodoController.deleteTodo);
+TodoRouter.get("/getTodos",TodoController.getAllTodos);
+
 
 export default TodoRouter;
